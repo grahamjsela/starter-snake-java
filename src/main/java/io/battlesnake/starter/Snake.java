f65d771dad5291703cae6f653458cf0007674f76
Graham Sela <grahamsela@Grahams-MacBook-Air.local>
2019-03-02 15:17:44 -0800

    asdf

--- src/main/java/io/battlesnake/starter/Snake.java
+++ src/main/java/io/battlesnake/starter/Snake.java
@@ -129,14 +129,14 @@ public class Snake {
             int myY = moveRequest.get("you").get("body").get("y").intValue();
             int boardX = moveRequest.get("board").get("width").intValue();
             int boardY = moveRequest.get("board").get("height").intValue();
-            if ((myX != 0 && myX != boardX) || (myY != 0 && myY != boardY)) {
+            if (myX != 0 && myX != boardX && myY != 0 && myY != boardY) {
 
               if (myX > (boardX - myX)) {
-                response.put("move", "left");
+                response.put("move", "right");
                 lastMove = "down";
 
               } else {
-                response.put("move", "right");
+                response.put("move", "left");
                 lastMove = "up";
 
               }
